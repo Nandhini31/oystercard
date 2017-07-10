b59719619256fc8d8e665490d6e0f8600409dd10
nandhini <nandhini31@gmail.com>
2017-07-10 15:09:18 +0100

    Challenge 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,8 +4,17 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:oyster) { described_class.new }
+  let(:amount) {50}
 
   it 'has balance of 0' do
     expect(oyster.balance).to eq(0)
+  end
+
+  describe '#top_up' do
+
+    # it {is_expected.to respond_to(:top_up).with(1).argument}
+    it 'top up to balance ' do
+    expect{oyster.top_up 50}.to change {oyster.balance}.by 50
+    end
   end
 end
